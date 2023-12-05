cb5971e7d621c5ef2b75da4273a097513120ce8a
0hr <harunrasit@pekacar.net>
2023-12-04 22:59:08 -0600

    removed token

--- responses/UserResponse.js
+++ responses/UserResponse.js
@@ -3,5 +3,4 @@ import BaseResponse from "./BaseResponse.js";
 export default class UserResponse extends BaseResponse {
     name = '';
     email = '';
-    token = '';
 }
--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,6 @@ userRouter.post('/register/email', registerValidations, async function (req, res
         const user = await userService.register(name, email, password);
         response.name = name;
         response.email = email;
-        response.token = await userService.createToken(user);
     } catch (err) {
         res.status(500);
         response.status.message = err.message;
@@ -52,44 +51,5 @@ userRouter.post('/register/email', registerValidations, async function (req, res
     return res.json(response);
 });
 
-const loginValidations = [
-    check('email')
-        .exists().withMessage('Email is required')
-        .isEmail().withMessage('Email is not valid'),
-    check('password')
-        .exists().withMessage('Password is required')
-        .isLength({min: 6}).withMessage('Password should be more than 6 characters')
-
-];
-
-userRouter.post('/login/email', loginValidations, async function (req, res, next) {
-    const response = new UserResponse();
-    try {
-        const validation = validationResult(req);
-        if (!validation.isEmpty()) {
-            res.status(403);
-            response.status.message = "validation error";
-            response.status.code = 403;
-            response.status.errors = validation;
-            return res.json(response);
-        }
-        const userService = new UserService();
-        const email = req.body.email;
-        const password = req.body.password;
-        const user = await userService.login(email, password);
-        if (!user) {
-            throw new Error('User is not found');
-        }
-
-        response.name = user.displayName;
-        response.email = email;
-        response.token = await userService.createToken(user);
-    } catch (err) {
-        res.status(500);
-        response.status.message = err.message;
-        response.status.code = 500;
-    }
-    return res.json(response);
-});
 
 export default userRouter;
