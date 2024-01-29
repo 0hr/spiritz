a49fab93b76733e789149e0d599b34428c676bb4
0hr <harunrasit@pekacar.net>
2024-01-29 15:52:44 -0600

    fixed prompt

--- services/IdentifierService.js
+++ services/IdentifierService.js
@@ -1,7 +1,6 @@
-import admin from "firebase-admin";
+import admin from 'firebase-admin';
 import OpenAI from 'openai';
-import {OPENAI_API_KEY} from "../consts.js";
-import indetifiers from "../routes/indetifiers.js";
+import {OPENAI_API_KEY} from '../consts.js';
 
 export default class IdentifierService {
 
@@ -17,7 +16,7 @@ export default class IdentifierService {
         const collection = db.collection('identifications');
         const result = await collection.get()
         if (result.empty) {
-            throw new Error("Collection is empty");
+            throw new Error('Collection is empty');
         }
 
         const list = [];
@@ -37,7 +36,7 @@ export default class IdentifierService {
         const collection = db.collection('identifications');
         const result = await collection.doc(id).get()
         if (!result.exists) {
-            throw new Error("Collection is empty");
+            throw new Error('Collection is empty');
         }
         const identifier = result.data();
 
@@ -48,12 +47,34 @@ export default class IdentifierService {
                 {
                     role: 'user',
                     content: [
-                        {type: 'text', text: "Execute the following instructions. Only perform the specified actions and refrain from any additional actions."},
-                        {type: 'text', text: identifier.prompt},
-                        {type: 'text', text: "If the given item is not found in the image, assign the status as 0; otherwise, assign it as 1. Present the results as a JSON format without markdown."},
-                        {type: 'text', text: "Use the 'status' field for the assigned status.  Place the answer in the 'answer' field as array. If multiple items are identified, include them in the 'answer' field as an array. " +
-                                "If no items are identified, add the answer is in answer field as an array.  Only Provide answer and status fields and provide only answers in 'answer' field as an array."},
-                        {type: 'text', text: `Provide the answer in the language indicated by the language code ${lang}, and refrain from translating JSON keys.`},
+                        {
+                            type: 'text',
+                            text: "Execute the prompt and following instructions. Only perform the specified actions and refrain from any additional actions."
+                        },
+                        {
+                            type: 'text',
+                            text: `Prompt: ${identifier.prompt}.`
+                        },
+                        {
+                            type: 'text',
+                            text: "If the given item is not found in the image, assign the status as 0; otherwise, assign it as 1. Present the results as a JSON format without markdown."
+                        },
+                        {
+                            type: 'text',
+                            text: "Use the 'status' field for the assigned status.  Place the answer in the 'answer' field as array. If multiple items are identified, include them in the 'answer' field as an array. "
+                        },
+                        {
+                            type: 'text',
+                            text: "If no items are identified, add the answer is in answer field as an array.  Only Provide answer and status fields and provide only answers in 'answer' field as an array."
+                        },
+                        {
+                            type: 'text',
+                            text: "Answered each item should not be json. Each item should be a text. "
+                        },
+                        {
+                            type: 'text',
+                            text: `Provide the answer in the language indicated by the language code ${lang}, and refrain from translating JSON keys.`
+                        },
                         {
                             type: 'image_url',
                             image_url: {
